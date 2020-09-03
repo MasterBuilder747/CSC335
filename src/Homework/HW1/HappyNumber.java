58c0ec09940d346f3b1323a37fd0963895d6595e
Joseph <jaathunder@yahoo.com>
2020-09-02 22:21:44 -0700

    implemented prime number check

--- src/Homework/HW1/HappyNumber.java
+++ src/Homework/HW1/HappyNumber.java
@@ -1,5 +1,12 @@
 package Homework.HW1;
 
 public class HappyNumber {
-    
+    public static boolean isHappy(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        } else {
+
+        }
+        return true;
+    }
 }
--- src/Homework/HW1/PrimeNumber.java
+++ src/Homework/HW1/PrimeNumber.java
@@ -1,5 +1,19 @@
 package Homework.HW1;
 
 public class PrimeNumber {
-
+    public static boolean isPrime(int n) throws IllegalArgumentException {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            if (n == 0 || n == 1) {
+                return false;
+            }
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
